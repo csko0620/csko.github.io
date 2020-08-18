4a24ef8c9355d1e1adc19dac61d769efdb4f4f28
csko0620 <68992649+csko0620@users.noreply.github.com>
2020-08-18 14:28:01 +0800

    new
    okay

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 var button = document.getElementById("create");
 var input = document.getElementById("input");
 var list = document.getElementById("list");
-var todolist = JSON.parse(localStorage.getItem("listItems")) || [];
+var todolist = JSON.parse(localStorage.getItem("list")) || [];
 /*parse出現了!!!!!!!*/
-
+render();
 
 function addData()
 {
-  if(input.value !=0)
-    {todolist.push(input.value);
-     
-     localStorage.setItem("list"/*設定處存器名稱*/,JSON.stringify/*以字串形式存起來
-     */(todolist));
-     /*把事項存起來部會消失*/
-  
-     input.value="";
-  render();
- /*如果說想要設定成空白自步行新增就在這邊新增*/}
+  if(input.value !="")
+    {
+      todolist.push(input.value);
+      input.value="";
+       render();
+ /*如果說想要設定成空白自步行新增就在這邊新增*/
+    }
   else if(input.value ==0)
   {
     list.innerHTML = "請輸入字元";
@@ -30,6 +27,7 @@ function addData()
 function del(i)
 {
   todolist.splice(i,1);
+  alert("確定要刪除嘛!?");
 render();
 }
 
@@ -41,7 +39,7 @@ var con="";
 for(var i=0;i<todolist.length;i++)
   {   
     con =con+"<div>"+todolist[i]+"</div>"+"<button class='dee' onclick='del("+i+")'>刪除事項</button>";
-    /*不適所有函數都在上面才能宣告，而是這邊就可以宣告了!!!*/
+    /*不適所有函數都在上面才能宣告，而是這邊就可以宣告了!!!delData(${i}還有這種寫法*/
   }
 list.innerHTML = con;
  
